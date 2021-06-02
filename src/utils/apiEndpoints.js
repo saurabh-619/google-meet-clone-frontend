6acea1331376b0a7345dd70d7d274e92f3cab34a
Saurabh Bomble <57832707+saurabh-619@users.noreply.github.com>
2021-06-03 00:28:15 +0530

    Update apiEndpoints.js

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://google-meet-clone-backend.herokuapp.com/";
+export const BASE_URL = "https://google-meet-clone-backend.herokuapp.com";
 export const SAVE_CALL_ID = "/api/save-call-id";
 export const GET_CALL_ID = "/api/get-call-id";
